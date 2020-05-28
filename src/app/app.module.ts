742264a1d3a651730cf675feb7c03418cb2b7558
Mariano Andres Botta <marianoabotta@gmail.com>
2020-05-28 19:56:17 -0300

    update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Plex, PlexModule } from '@andes/plex';
 import { routing, appRoutingProviders } from './app-routing.module';
+import { SharedModule } from '@andes/shared';
+
 
 // Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
 
 @NgModule({
- declarations: [
-   AppComponent,
-   HomeComponent
- ],
- imports: [
-   BrowserModule,
-   FormsModule,
-   HttpModule,
-   PlexModule,
-   routing
- ],
- providers: [
-   Plex,  
-   appRoutingProviders  
- ],
- bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    PlexModule,
+    SharedModule,
+    routing
+  ],
+  providers: [
+    Plex,
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
